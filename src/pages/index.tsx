08702e3fbda503676fdde0cc8661513a142c66f9
MarcosDMT <marcosgav80@gmail.com>
2023-08-11 09:45:52 +0300

    modified the index component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
-
 export default function Home() {
   return (
     <>
-     Welcome
+      <section className="p-4 space-y-4 bg-[#DFEAF0] pb-12 h-[89vh]">
+        <div className="md:flex items-center gap-4 md:space-y-0 space-y-2">
+          <div className="bg-[#272684] text-white flex items-center justify-center rounded shadow-2xl md:h-28 h-16 md:w-3/12 w-full">
+            No. of Beneficiaries
+          </div>
+          <div className="bg-[#272684] text-white flex items-center justify-center rounded shadow-2xl md:h-28 h-16 md:w-3/12 w-full">
+            No. of Beneficiaries
+          </div>
+          <div className="bg-[#272684] text-white flex items-center justify-center md:h-28 h-16 rounded shadow-2xl md:w-3/12 w-full">
+            No. of Beneficiaries
+          </div>
+          <div className="bg-[#272684] text-white flex items-center justify-center rounded shadow-2xl md:h-28 h-16 md:w-3/12 w-full">
+            No. of Beneficiaries
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
