a6cb6a9580d11e48dcb9704cc932194d9392f04d
MarcosDMT <marcosgav80@gmail.com>
2023-08-16 10:21:08 +0300

    modified the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ type props = {
 const Navbar = ({ collapsed }: props) => {
   const [isOpen, setIsOpen] = useState(false);
   const [navBackground, setNavBackground] = useState(false);
+  const [show, setShow] = useState(false);
 
   const navRef = useRef<boolean>();
   navRef.current = navBackground;
@@ -42,11 +43,18 @@ const Navbar = ({ collapsed }: props) => {
             <Drawer {...{ isOpen, setIsOpen, collapsed }} />
           </div>
           <img
+            onClick={() => setShow((prev) => !prev)}
             className="w-10 h-10 rounded-full cursor-pointer"
             src="/images/me.png"
             alt=""
           />
         </div>
+        {show && (
+          <div className="bg-white px-2 space-y-2 py-4 shadow-2xl absolute top-14 sm:right-10 right-4 rounded w-24 h-28 text-black">
+            <p className="">Profile</p>
+            <p className="">Logout</p>
+          </div>
+        )}
       </section>
     </>
   );
